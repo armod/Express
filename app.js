76c8dd11ba07f5085a5dc17175e52707d02f9a21
armod <armods@gmail.com>
2022-11-18 21:34:16 +0100

    express

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ const app = express()
 //setup static and middleware
 app.use(express.static('./public'))
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-})
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+// adding to static assets
+// SSR
+// })
+
 app.all('*', (req, res) => {
   req.status(404).send('resource not found')
 })
 
 app.listen(5000, () => {
-  console.log('Server start on port 5000....')
+  console.log('Server listening on port 5000....')
 })
