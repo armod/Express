9568baf8b691f7ac5c6ae5700e8d62ed49fa6b9d
armod <armods@gmail.com>
2022-11-18 21:17:02 +0100

    express

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const app = express()
+
+//app.get
+app.get('/', (req, res) => {
+  console.log('user hit the resource')
+  res.send('<h1>Home Page</h1>')
+})
+
+app.get('/about', (req, res) => {
+  res.send('About Page')
+})
+
+app.all('*', (req, res) => {
+  res.send('<h1>Resource not found</h1>')
+})
+
 app.listen(5000, () => {
   console.log('server is listening on port 5000')
 })
-//app.get
 //app.post
 //app.put
 //app.delete
