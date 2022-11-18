a51b63f1d97a9652562fb093a4b75bb5e186085f
armod <armods@gmail.com>
2022-11-18 21:25:36 +0100

    express

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
-//app.get
-//app.post
-//app.put
-//app.delete
-//app.all
-//app.use
-//app.listen
+app.use(express.static('./public'))
 
 app.get('/', (req, res) => {
-  console.log('user hit the resource')
-  res.status(200).send('<h1>Home Page</h1>')
+  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
 })
-
-app.get('/about', (req, res) => {
-  res.status(200).send('About Page')
-})
-
 app.all('*', (req, res) => {
-  res.status(404).send('<h1>Resource not found</h1>')
+  req.status(404).send('resource not found')
 })
 
 app.listen(5000, () => {
-  console.log('server is listening on port 5000')
+  console.log('Server start on port 5000....')
 })
