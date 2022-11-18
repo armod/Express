ed43e9f61021fcb97c8fd959957071125f1d31cc
armod <armods@gmail.com>
2022-11-18 21:19:42 +0100

    express

--- app.js
+++ app.js
@@ -2,25 +2,26 @@ const express = require('express')
 const app = express()
 
 //app.get
-app.get('/', (req, res) => {
-  console.log('user hit the resource')
-  res.send('<h1>Home Page</h1>')
-})
-
-app.get('/about', (req, res) => {
-  res.send('About Page')
-})
-
-app.all('*', (req, res) => {
-  res.send('<h1>Resource not found</h1>')
-})
-
-app.listen(5000, () => {
-  console.log('server is listening on port 5000')
-})
 //app.post
 //app.put
 //app.delete
 //app.all
 //app.use
 //app.listen
+
+app.get('/', (req, res) => {
+  console.log('user hit the resource')
+  res.status(200).send('<h1>Home Page</h1>')
+})
+
+app.get('/about', (req, res) => {
+  res.status(200).send('About Page')
+})
+
+app.all('*', (req, res) => {
+  res.status(404).send('<h1>Resource not found</h1>')
+})
+
+app.listen(5000, () => {
+  console.log('server is listening on port 5000')
+})
