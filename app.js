0157f10011092dc0d078727bb747386ef91f8e05
armod <armods@gmail.com>
2022-11-19 17:44:09 +0100

    express2

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
+const { products } = require('./data')
 
 app.get('/', (req, res) => {
-  res.json([{ name: 'Maciej' }, { name: 'John' }])
+  res.json(products)
 })
 
 app.listen(5000, (req, res) => {
