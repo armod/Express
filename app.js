87ad65539152e1df4f068970dbc69a214b045139
armod <armods@gmail.com>
2022-11-19 18:44:06 +0100

    express2

--- app.js
+++ app.js
@@ -14,6 +14,20 @@ app.get('/api/products', (req, res) => {
   res.json(newProducts)
 })
 
+app.get('/api/products/:productID', (req, res) => {
+  // console.log(req)
+  // console.log(req.params)
+  const { productID } = req.params
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  )
+  if (!singleProduct) {
+    return res.status(404).send('Product does not exist')
+  }
+  console.log(singleProduct)
+  return res.json(singleProduct)
+})
+
 app.listen(5000, (req, res) => {
   console.log('Server listening on port 5000...')
 })
