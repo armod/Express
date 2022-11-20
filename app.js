dc57091de6e47dc2751e673c8820ea1379518c2d
armod <armods@gmail.com>
2022-11-20 17:17:49 +0100

    express GET

--- app.js
+++ app.js
@@ -1,34 +1,11 @@
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
-const logger = require('./logger')
-const authorize = require('./authorize')
-// req => middleware => res
-// app.use('/api', logger)
-// app.use([logger, authorize])
-// app.use(express.static('./public'))
-app.use(morgan('tiny'))
-// api/home/about/products
-app.get('/', (req, res) => {
-  res.send('Home')
-  // const method = req.method
-  // const url = req.url
-  // const time = new Date().getFullYear()
-  // console.log(method, url, time)
-})
+let { people } = require('./data')
 
-app.get('/about', (req, res) => {
-  res.send('About')
-})
-app.get('/api/products', (req, res) => {
-  res.send('Products')
-})
-// app.get('/api/items', [logger, authorize], (req, res) => {
-app.get('/api/items', (req, res) => {
-  console.log(req.user)
-  res.send('Items')
+app.get('/api/people', (req, res) => {
+  res.status(200).json({ success: true, data: people })
 })
 
 app.listen(5000, () => {
-  console.log('Server listening on port 5000...')
+  console.log('Server listening at port 5000...')
 })
